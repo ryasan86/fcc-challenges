a7827afb9d62da403b6eee9ece53c9f757ea3230
Ryan Santos <ryansantos86@gmail.com>
2017-09-21 14:49:06 -0700

    first commit

---
+++ whereDoIBelong.js
@@ -0,0 +1,20 @@
+function getIndexToIns(arr, num) {
+  // var tempArr = [];
+
+  arr.sort(function(a, b) {
+    return a - b;
+  });
+
+  for (var i = 0; i < arr.length; i++) {
+    // tempArr.push(arr[i]);
+    var numIndex = arr.indexOf(arr[i]);
+
+    if (arr[i] >= num) {
+      return i;
+      // arr.splice(i, 0, num);
+    }
+  }
+  return arr.length;
+}
+
+console.log(getIndexToIns([10, 20, 30, 40, 50], 35));
