f00a4afffe62edee81ddfff97d6bfa0534c245aa
Ryan Santos <ryansantos86@gmail.com>
2017-11-13 18:17:59 -0800

    commit

---
+++ src/intermediate_algorithm_scripting/missing-letters.js
@@ -0,0 +1,22 @@
+function fearNotLetter(str) {
+  var start = str.charCodeAt(0);
+  var end = str.charCodeAt(str.length - 1);
+
+  for (var i = start; i <= end; i++) {
+    var index = str.indexOf(String.fromCharCode(i));
+
+    if (!str[index]) {
+      return String.fromCharCode(i);
+    }
+  }
+}
+
+function assertEqual(actual, expected) {
+  if (actual === expected) {
+    console.log('PASSED');
+  } else {
+    console.log('FAILED');
+  }
+}
+
+assertEqual(fearNotLetter('abcdefghjklmno'), 'i');
