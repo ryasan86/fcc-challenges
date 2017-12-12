e368ff72cfe61129aeb76d2734b0f8f26c568cd8
Ryan Santos <ryansantos86@gmail.com>
2017-12-11 17:31:31 -0800

    commit

---
+++ src/intermediate_algorithm_scripting/steamroller.js
@@ -0,0 +1,27 @@
+function assertArrayEquals(actual, expected) {
+  var areEqualLength = actual.length === expected.length;
+  var areEqualItems = actual.every(function(item, i) {
+    return item === expected[i];
+  });
+
+  if (areEqualLength && areEqualItems) {
+    console.log('PASSED');
+  } else {
+    console.log('FAILED');
+  }
+}
+
+function steamrollArray(arr) {
+  var flattened = [];
+  arr.forEach(function(el) {
+    if (Array.isArray(el)) {
+      flattened = flattened.concat(steamrollArray(el));
+    } else {
+      flattened.push(el);
+    }
+  });
+
+  return flattened;
+}
+
+assertArrayEquals(steamrollArray([[['a']], [['b']]]), ['a', 'b']);
