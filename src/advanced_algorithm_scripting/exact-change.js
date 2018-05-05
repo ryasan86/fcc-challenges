96606af2523346a9b1d023d03cc82af92b2671c3
Ryan Santos <ryansantos86@gmail.com>
2018-05-05 16:55:44 -0700

    commit

--- src/advanced_algorithm_scripting/exact-change.js
+++ src/advanced_algorithm_scripting/exact-change.js
@@ -10,81 +10,64 @@ const money = {
   'ONE HUNDRED': 100
 };
 
-function checkCashRegister(
-  price,
-  cash,
-  cid,
-  cidReversed,
-  changeDue,
-  change = []
-) {
-  if (!cidReversed) {
-    cid = cid.filter(item => {
-      return item[1] !== 0;
-    });
-    cid = cid.reverse();
-    cidReversed = true;
-  }
+function checkCashRegister(price, cash, cid) {
+  let change = [];
+  let changeDue = cash - price;
+  cid = cid.reverse();
 
-  changeDue = changeDue || cash - price;
-  
-  let currBill = cid.find(bill => {
-    return money[bill[0]] <= changeDue;
+  cid.forEach(c => {
+    const moneyName = c[0];
+    let leftInDrawer = c[1];
+    const moneyVal = money[moneyName];
+
+    if (moneyVal <= changeDue && changeDue > 0) {
+      const fromDrawer = drainDrawer(moneyVal, leftInDrawer, changeDue);
+      change.push([moneyName, fromDrawer]);
+      changeDue -= fromDrawer;
+      c[1] -= fromDrawer;
+    }
   });
-  
-  let result = [currBill[0], 0];
-  
-  while (changeDue >= money[currBill[0]] && currBill[1] > 0) {
-    changeDue = Math.round(changeDue * 100) / 100;
-    result[1] += money[currBill[0]];
-    currBill[1] -= money[currBill[0]];
-    changeDue -= money[currBill[0]];
-  }
-  
-  if (currBill[1] === 0) {
-    const index = cid.indexOf(currBill);
-    cid.splice(index, 1);
-  }
 
-  change.push(result);
-
-  if (changeDue === 0 && !cid) {
-
-  }
+  return checkChangeDue(changeDue, cid) || change;
 }
 
-let y = checkCashRegister(19.5, 20.0, [
-  ['PENNY', 0.5],
+const drainDrawer = (moneyVal, leftInDrawer, changeDue) => {
+  let result = 0;
+
+  while (leftInDrawer > 0 && changeDue > 0 && changeDue - moneyVal >= 0) {
+    changeDue -= moneyVal;
+    leftInDrawer -= moneyVal;
+    result += moneyVal;
+    leftInDrawer = Math.round(leftInDrawer * 100) / 100;
+    changeDue = Math.round(changeDue * 100) / 100;
+    result = Math.round(result * 100) / 100;
+  }
+
+  return result;
+};
+
+const checkChangeDue = (changeDue, cid) => {
+  if (changeDue === 0 && checkCID(cid)) {
+    return 'Closed';
+  } else if (changeDue > 0) {
+    return 'Insufficient Funds';
+  }
+};
+
+const checkCID = cid => {
+  return cid.every(c => c[1] === 0);
+};
+
+const a = checkCashRegister(19.5, 20.0, [
+  ['PENNY', 0.01],
   ['NICKEL', 0],
   ['DIME', 0],
   ['QUARTER', 0],
-  ['ONE', 0],
+  ['ONE', 1.0],
   ['FIVE', 0],
   ['TEN', 0],
   ['TWENTY', 0],
   ['ONE HUNDRED', 0]
 ]);
 
-console.log(y);
-// let x = checkCashRegister(3.26, 100.0, [
-//   ['PENNY', 1.01],
-//   ['NICKEL', 2.05],
-//   ['DIME', 3.1],
-//   ['QUARTER', 4.25],
-//   ['ONE', 90.0],
-//   ['FIVE', 55.0],
-//   ['TEN', 20.0],
-//   ['TWENTY', 60.0],
-//   ['ONE HUNDRED', 100.0]
-// ]);
-// console.log(x);
-// [
-//   // should return
-//   (['TWENTY', 60.0],
-//   ['TEN', 20.0],
-//   ['FIVE', 15.0],
-//   ['ONE', 1.0],
-//   ['QUARTER', 0.5],
-//   ['DIME', 0.2],
-//   ['PENNY', 0.04])
-// ];
+console.log(a);
